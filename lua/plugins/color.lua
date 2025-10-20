6e0b03bef7faa2419c31d919a080dc60191f3270
Maicon <maicon.rodrigues017@proton.me>
2025-10-20 15:49:52 -0300

    colorscheme

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,54 +1,218 @@
+local background = "#000000"
+local foreground = "#00ffbf"
+local fg_light_30 = "#009973"
+local fg_light_40 = "#00cc99"
+local fg_light_70 = "#66ffd9"
+
 return {
 	"rktjmp/lush.nvim",
 	config = function()
 		local lush = require("lush")
-		local hsl = lush.hsl
+		-- local hsl = lush.hsl
 		---@diagnostic disable: undefined-global
 		local theme = lush(function(injected_functions)
 			local sym = injected_functions.sym
 			return {
-				Normal({ bg = "#0a0a0a", fg = "#00ff80" }), -- Normal text
-				ColorColumn({ bg = "#0a0a0a" }), -- Columns set with 'colorcolumn'
+				-- UI
+				ColorColumn({ bg = background }), -- Columns set with 'colorcolumn'
+				Conceal({ fg = foreground }), -- Placeholder characters substituted for concealed text (see 'conceallevel')
+				-- Cursor         { }, -- Character under the cursor
+				-- CurSearch      { }, -- Highlighting a search pattern under the cursor (see 'hlsearch')
+				-- lCursor        { }, -- Character under the cursor when |language-mapping| is used (see 'guicursor')
+				-- CursorIM       { }, -- Like Cursor, but used when in IME mode |CursorIM|
+				CursorColumn({ bg = background }), -- Screen-column at the cursor, when 'cursorcolumn' is set.
+				CursorLine({ bg = background }), -- Screen-line at the cursor, when 'cursorline' is set. Low-priority if foreground (ctermfg OR guifg) is not set.
+				Directory({ bg = background, fg = foreground }), -- Directory names (and other special names in listings)
+				-- DiffAdd        { }, -- Diff mode: Added line |diff.txt|
+				-- DiffChange     { }, -- Diff mode: Changed line |diff.txt|
+				-- DiffDelete     { }, -- Diff mode: Deleted line |diff.txt|
+				-- DiffText       { }, -- Diff mode: Changed text within a changed line |diff.txt|
+				EndOfBuffer({ fg = foreground }), -- Filler lines (~) after the end of the buffer. By default, this is highlighted like |hl-NonText|.
+				-- TermCursor     { }, -- Cursor in a focused terminal
+				-- TermCursorNC   { }, -- Cursor in an unfocused terminal
+				-- ErrorMsg       { }, -- Error messages on the command line
+				-- VertSplit      { }, -- Column separating vertically split windows
+				-- Folded         { }, -- Line used for closed folds
+				-- FoldColumn     { }, -- 'foldcolumn'
+				-- SignColumn     { }, -- Column where |signs| are displayed
+				IncSearch({ bg = foreground, fg = background }), -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+				Substitute({ bg = foreground, fg = background }), -- |:substitute| replacement text highlighting
+				LineNr({ fg = foreground }), -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+				LineNrAbove({ fg = foreground }), -- Line number for when the 'relativenumber' option is set, above the cursor line
+				LineNrBelow({ fg = foreground }), -- Line number for when the 'relativenumber' option is set, below the cursor line
+				-- CursorLineNr   { }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
+				-- CursorLineFold { }, -- Like FoldColumn when 'cursorline' is set for the cursor line
+				-- CursorLineSign { }, -- Like SignColumn when 'cursorline' is set for the cursor line
+				-- MatchParen     { }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+				ModeMsg({ fg = foreground }), -- 'showmode' message (e.g., "-- INSERT -- ")
+				MsgArea({ fg = foreground }), -- Area for messages and cmdline
+				-- MsgSeparator   { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
+				-- MoreMsg        { }, -- |more-prompt|
+				-- NonText        { }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
+				Normal({ bg = background, fg = foreground }), -- Normal text
+				-- NormalFloat    { }, -- Normal text in floating windows.
+				-- FloatBorder    { }, -- Border of floating windows.
+				-- FloatTitle     { }, -- Title of floating windows.
+				-- NormalNC       { }, -- normal text in non-current windows
+				Pmenu({ bg = background, fg = foreground }), -- Popup menu: Normal item.
+				-- PmenuSel       { }, -- Popup menu: Selected item.
+				-- PmenuKind      { }, -- Popup menu: Normal item "kind"
+				-- PmenuKindSel   { }, -- Popup menu: Selected item "kind"
+				-- PmenuExtra     { }, -- Popup menu: Normal item "extra text"
+				-- PmenuExtraSel  { }, -- Popup menu: Selected item "extra text"
+				-- PmenuSbar      { }, -- Popup menu: Scrollbar.
+				-- PmenuThumb     { }, -- Popup menu: Thumb of the scrollbar.
+				Question({ bg = foreground }), -- |hit-enter| prompt and yes/no questions
+				-- QuickFixLine   { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
+				-- Search         { }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
+				SpecialKey({ fg = fg_light_70 }), -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
+				-- SpellBad       { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
+				-- SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
+				-- SpellLocal     { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
+				-- SpellRare      { }, -- Word that is recognized by the spellchecker as one that is hardly ever used. |spell| Combined with the highlighting used otherwise.
+				StatusLine({ fg = foreground }), -- Status line of current window
+				StatusLineNC({ fg = foreground }), -- Status lines of not-current windows. Note: If this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
+				TabLine({ bg = background, fg = foreground }), -- Tab pages line, not active tab page label
+				-- TabLineFill    { }, -- Tab pages line, where there are no labels
+				TabLineSel({ bg = foreground, fg = background }), -- Tab pages line, active tab page label
+				Title({ fg = foreground }), -- Titles for output from ":set all", ":autocmd" etc.
+				Visual({ fg = background, bg = fg_light_40 }), -- Visual mode selection
+				-- VisualNOS      { }, -- Visual mode selection when vim is "Not Owning the Selection".
+				WarningMsg({ fg = foreground }), -- Warning messages
+				-- Whitespace     { }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
+				-- Winseparator   { }, -- Separator between window splits. Inherts from |hl-VertSplit| by default, which it will replace eventually.
+				-- WildMenu       { }, -- Current match in 'wildmenu' completion
+				-- WinBar         { }, -- Window bar of current window
+				-- WinBarNC       { }, -- Window bar of not-current windows
 
-				Pmenu({ bg = "#0a0a0a", fg = "#00ff80" }), -- Popup menu: Normal item.
+				-- SYNTAX
+				Comment({ fg = fg_light_30 }), -- Any comment
+				Constant({ fg = fg_light_70 }), -- (*) Any constant
+				String({ fg = fg_light_40 }), --   A string constant: "this is a string"
+				-- Character      { }, --   A character constant: 'c', '\n'
+				-- Number         { }, --   A number constant: 234, 0xff
+				-- Boolean        { }, --   A boolean constant: TRUE, false
+				-- Float          { }, --   A floating point constant: 2.3e10
 
-				Directory({ fg = "#00ff80" }), -- Directory names (and other special names in listings)
+				Identifier({ fg = foreground }), -- (*) Any variable name
+				Function({ fg = foreground }), --   Function name (also: methods for classes)
 
-				LineNr({ fg = "#00ff80" }), -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
-				LineNrAbove({ fg = "#00ff80" }), -- Line number for when the 'relativenumber' option is set, above the cursor line
-				LineNrBelow({ fg = "#00ff80" }), -- Line number for when the 'relativenumber' option is set, below the cursor line
-				Conceal({ fg = "#00ff80" }), -- Placeholder characters substituted for concealed text (see 'conceallevel')
-				Title({ fg = "#00ff80" }), -- Titles for output from ":set all", ":autocmd" etc.
-				CursorLine({ bg = "#111111" }), -- Screen-line at the cursor, when 'cursorline' is set. Low-priority if foreground (ctermfg OR guifg) is not set.
+				Statement({ fg = foreground, gui = "bold" }), -- (*) Any statement
+				-- Conditional    { }, --   if, then, else, endif, switch, etc.
+				-- Repeat         { }, --   for, do, while, etc.
+				-- Label          { }, --   case, default, etc.
+				Operator({ fg = foreground }), --   "sizeof", "+", "*", etc.
+				-- Keyword        { }, --   any other keyword
+				-- Exception      { }, --   try, catch, throw
 
-				ModeMsg({ fg = "#00ff80" }), -- 'showmode' message (e.g., "-- INSERT -- ")
-				MsgArea({ fg = "#00ff80" }), -- Area for messages and cmdline
-				WarningMsg({ fg = "#00ff80" }), -- Warning messages
-				Question({ bg = "#00ff80" }), -- |hit-enter| prompt and yes/no questions
+				PreProc({ fg = foreground }), -- (*) Generic Preprocessor
+				-- Include        { }, --   Preprocessor #include
+				-- Define         { }, --   Preprocessor #define
+				-- Macro          { }, --   Same as Define
+				-- PreCondit      { }, --   Preprocessor #if, #else, #endif, etc.
 
-				StatusLine({ fg = "#00ff80" }), -- Status line of current window
-				StatusLineNC({ fg = "#00ff80" }), -- Status lines of not-current windows. Note: If this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
+				Type({ fg = foreground }), -- (*) int, long, char, etc.
+				-- StorageClass   { }, --   static, register, volatile, etc.
+				-- Structure      { }, --   struct, union, enum, etc.
+				-- Typedef        { }, --   A typedef
 
-				Visual({ bg = "#1d754f" }), -- Visual mode selection
-				IncSearch({ bg = "#1d754f", fg = "#00ff80" }), -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+				Special({ fg = foreground }), -- (*) Any special symbol
+				-- SpecialChar    { }, --   Special character in a constant
+				-- Tag            { }, --   You can use CTRL-] on this
+				Delimiter({ fg = foreground }), --   Character that needs attention
+				-- SpecialComment { }, --   Special things inside a comment (e.g. '\n')
+				-- Debug          { }, --   Debugging statements
 
-				Function({ fg = "#00ff80" }), --   Function name (also: methods for classes)
-				Statement({ fg = "#00ff80", gui = "bold" }), -- (*) Any statement
-				Type({ fg = "#00ff80" }), -- (*) int, long, char, etc.
-				sym("@variable")({ fg = "#4dffa5" }), -- Identifier
-				String({ fg = "#4dffa5" }), --   A string constant: "this is a string"
-				Comment({ fg = "#00b300" }), -- Any comment
-				PreProc({ fg = "#00ff80" }), -- (*) Generic Preprocessor
-				Constant({ fg = "#4dffa5" }), -- (*) Any constant
-				Special({ fg = "#00ff80" }), -- (*) Any special symbol
-				Identifier({ fg = "#00ff80" }), -- (*) Any variable name
-				Operator({ fg = "#00ff80" }), --   "sizeof", "+", "*", etc.
-				Delimiter({ fg = "#00ff80" }), --   Character that needs attention
-				DiagnosticError({ fg = "#00ff80" }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-				DiagnosticHint({ fg = "#00ff80" }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-				DiagnosticVirtualTextWarn({ fg = "#00ff80" }), -- Used for "Warn" diagnostic virtual text.
-				DiagnosticVirtualTextHint({ fg = "#00ff80" }), -- Used for "Hint" diagnostic virtual text.
-				DiagnosticInfo({ fg = "#4dffa5" }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+				-- Underlined     { gui = "underline" }, -- Text that stands out, HTML links
+				-- Ignore         { }, -- Left blank, hidden |hl-Ignore| (NOTE: May be invisible here in template)
+				-- Error          { }, -- Any erroneous construct
+				-- Todo           { }, -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+
+				-- LSP/DIAGNOSTICS
+				-- LspReferenceText            { } , -- Used for highlighting "text" references
+				-- LspReferenceRead            { } , -- Used for highlighting "read" references
+				-- LspReferenceWrite           { } , -- Used for highlighting "write" references
+				-- LspCodeLens                 { } , -- Used to color the virtual text of the codelens. See |nvim_buf_set_extmark()|.
+				-- LspCodeLensSeparator        { } , -- Used to color the seperator between two or more code lens.
+				-- LspSignatureActiveParameter { } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
+
+				DiagnosticError({ fg = foreground }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+				-- DiagnosticWarn             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+				DiagnosticInfo({ fg = foreground }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+				DiagnosticHint({ fg = foreground }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+				-- DiagnosticOk               { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+				-- DiagnosticVirtualTextError { } , -- Used for "Error" diagnostic virtual text.
+				DiagnosticVirtualTextWarn({ fg = foreground }), -- Used for "Warn" diagnostic virtual text.
+				-- DiagnosticVirtualTextInfo  { } , -- Used for "Info" diagnostic virtual text.
+				DiagnosticVirtualTextHint({ fg = foreground }), -- Used for "Hint" diagnostic virtual text.
+				-- DiagnosticVirtualTextOk    { } , -- Used for "Ok" diagnostic virtual text.
+				-- DiagnosticUnderlineError   { } , -- Used to underline "Error" diagnostics.
+				-- DiagnosticUnderlineWarn    { } , -- Used to underline "Warn" diagnostics.
+				-- DiagnosticUnderlineInfo    { } , -- Used to underline "Info" diagnostics.
+				-- DiagnosticUnderlineHint    { } , -- Used to underline "Hint" diagnostics.
+				-- DiagnosticUnderlineOk      { } , -- Used to underline "Ok" diagnostics.
+				-- DiagnosticFloatingError    { } , -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
+				-- DiagnosticFloatingWarn     { } , -- Used to color "Warn" diagnostic messages in diagnostics float.
+				-- DiagnosticFloatingInfo     { } , -- Used to color "Info" diagnostic messages in diagnostics float.
+				-- DiagnosticFloatingHint     { } , -- Used to color "Hint" diagnostic messages in diagnostics float.
+				-- DiagnosticFloatingOk       { } , -- Used to color "Ok" diagnostic messages in diagnostics float.
+				-- DiagnosticSignError        { } , -- Used for "Error" signs in sign column.
+				-- DiagnosticSignWarn         { } , -- Used for "Warn" signs in sign column.
+				-- DiagnosticSignInfo         { } , -- Used for "Info" signs in sign column.
+				-- DiagnosticSignHint         { } , -- Used for "Hint" signs in sign column.
+				-- DiagnosticSignOk           { } , -- Used for "Ok" signs in sign column.
+
+				-- TREESITTER
+				-- sym("@text.literal")
+				-- sym('@text.literal')
+				-- sym"@text.literal"
+				-- sym'@text.literal'
+
+				-- sym"@text.literal"      { }, -- Comment
+				-- sym"@text.reference"    { }, -- Identifier
+				-- sym"@text.title"        { }, -- Title
+				-- sym"@text.uri"          { }, -- Underlined
+				-- sym"@text.underline"    { }, -- Underlined
+				-- sym"@text.todo"         { }, -- Todo
+				-- sym"@comment"           { }, -- Comment
+				-- sym"@punctuation"       { }, -- Delimiter
+				-- sym"@constant"          { }, -- Constant
+				-- sym"@constant.builtin"  { }, -- Special
+				-- sym"@constant.macro"    { }, -- Define
+				-- sym"@define"            { }, -- Define
+				-- sym"@macro"             { }, -- Macro
+				-- sym"@string"            { }, -- String
+				-- sym"@string.escape"     { }, -- SpecialChar
+				-- sym"@string.special"    { }, -- SpecialChar
+				-- sym"@character"         { }, -- Character
+				-- sym"@character.special" { }, -- SpecialChar
+				-- sym"@number"            { }, -- Number
+				-- sym"@boolean"           { }, -- Boolean
+				-- sym"@float"             { }, -- Float
+				-- sym"@function"          { }, -- Function
+				-- sym"@function.builtin"  { }, -- Special
+				-- sym"@function.macro"    { }, -- Macro
+				-- sym"@parameter"         { }, -- Identifier
+				-- sym"@method"            { }, -- Function
+				-- sym"@field"             { }, -- Identifier
+				-- sym"@property"          { }, -- Identifier
+				-- sym"@constructor"       { }, -- Special
+				-- sym"@conditional"       { }, -- Conditional
+				-- sym"@repeat"            { }, -- Repeat
+				-- sym"@label"             { }, -- Label
+				-- sym"@operator"          { }, -- Operator
+				-- sym"@keyword"           { }, -- Keyword
+				-- sym"@exception"         { }, -- Exception
+				sym("@variable")({ fg = fg_light_70 }), -- Identifier
+				-- sym"@type"              { }, -- Type
+				-- sym"@type.definition"   { }, -- Typedef
+				-- sym"@storageclass"      { }, -- StorageClass
+				-- sym"@structure"         { }, -- Structure
+				-- sym"@namespace"         { }, -- Identifier
+				-- sym"@include"           { }, -- Include
+				-- sym"@preproc"           { }, -- PreProc
+				-- sym"@debug"             { }, -- Debug
+				-- sym"@tag"               { }, -- Tag
 			}
 		end)
 		lush(theme)
