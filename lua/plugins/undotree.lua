c2fca08efb4acbe4f8a6ab6f8a69666596f65ff2
maicon1998 <135352259+maicon1998@users.noreply.github.com>
2026-02-22 14:01:14 +0000

    Update undotree.lua

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,8 +1,7 @@
 return {
-    "mbbill/undotree",
-
-    config = function() 
-        vim.g.undotree_DiffCommand = "FC"
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end
+	"mbbill/undotree",
+	config = function()
+		-- vim.g.undotree_DiffCommand = "FC" -- windows only
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+	end,
 }
